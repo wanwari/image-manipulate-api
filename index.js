d12825ab128ac6eceb5c27ac0729c47c0a77fa86
Wiesa Anwari <wiesa@wanwari.ca>
2022-06-28 13:06:02 -0400

    feat: upload file and save to server

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
+app.post("/upload", (req, res) => {
+	try {
+		if (!req.files) {
+			res.send({
+				uploaded: false,
+				responseMessage: "Error. File was not uploaded",
+			});
+		} else {
+			const uploadedImage = req.files.image;
+			uploadedImage.mv("./img/" + uploadedImage.name);
+			console.log(uploadedImage.name);
+
+			res.send({
+				uploaded: true,
+				responseMessage: "Success. File was uploaded",
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
+});
+
 app.listen(PORT, () => {
 	console.log(`[SERVER] listening on port ${PORT}`);
 });
