6bea10ecf6c599be0561353a880f4e80f87f363b
Wiesa Anwari <wiesa@wanwari.ca>
2022-06-29 12:43:25 -0400

    feat: manipulate image based on properties from POST request

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import sharp from "sharp";
 import fs from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { stringify } from "querystring";
 
 const app = express();
 
@@ -19,16 +18,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const PORT = process.env.PORT || 3000;
 
-const resizeImage = async (imageBuffer, size) => {
+const resizeImage = async (imageBuffer, properties) => {
 	let resized = sharp(imageBuffer)
 		.resize({
-			width: size.width,
-			height: size.height,
+			width: properties.width,
+			height: properties.height,
 			fit: "contain",
-			background: { r: 0, g: 255, b: 0, alpha: 1 },
+			background: {
+				r: properties.colour.r,
+				g: properties.colour.g,
+				b: properties.colour.b,
+				alpha: properties.colour.a,
+			},
 		})
 		.extend({ background: "red" })
-		.toFile("./processed/new.webp");
+		.toFile(`./processed/new.${properties.format}`);
 	return resized;
 };
 
@@ -40,20 +44,34 @@ app.post("/upload", (req, res) => {
 				responseMessage: "Error. File was not uploaded",
 			});
 		} else {
+			console.log(req.body.width);
 			const uploadedImage = req.files.image;
 			uploadedImage.mv("./img/" + uploadedImage.name).then(() => {
 				const buffer = fs.readFileSync("./img/" + uploadedImage.name);
 
-				resizeImage(buffer, {
-					width: 100,
-					height: 500,
-				}).then((response) => {
+				const properties = {
+					format: req.body.format,
+					width: parseInt(req.body.width),
+					height: parseInt(req.body.height),
+					colour: {
+						r: parseInt(req.body.r),
+						g: parseInt(req.body.g),
+						b: parseInt(req.body.b),
+						a: parseInt(req.body.a),
+					},
+				};
+
+				console.log(properties.colour);
+				resizeImage(buffer, properties).then((response) => {
 					console.log(response);
 
 					fs.unlink("./img/" + uploadedImage.name, () => {
-						res.status(202).sendFile("/processed/new.webp", {
-							root: __dirname,
-						});
+						res.status(202).sendFile(
+							`/processed/new.${properties.format}`,
+							{
+								root: __dirname,
+							}
+						);
 					});
 				});
 			});
